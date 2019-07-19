5ebae9a2be1fc618afd4fc726a9ce2bf6a945efc
Logan Cundiff <lcundiff@ufl.edu>
2019-07-19 17:04:53 -0400

    Revert "Revert "profile edits""
    This reverts commit 268a8088241ecc4b911b425ffc0016a1c96972d9.

--- profile_controller.py
+++ profile_controller.py
@@ -4,19 +4,11 @@ from flask import render_template
 
 
 
-@app.route('/')
-@app.route('/home')
-def index():
-    #url_for('static', filename='css/bootstrap.css')
-    #url_for('static', filename='css/bootstrap.min.css')
-    #url_for('static', filename='js/bootstrap.min.js')
-    #url_for('static', filename='js/jquery.min.js')
-    #url_for('static', filename='js/bootstrap.bundle.js')
-    return render_template('home.html', home_active="class=active")
 
-#@app.route('/profile')
-def map():
-    return render_template('profile.html', profile_active="class=active")
+@app.route('/')
+def hello_name():    
+  return render_template('profile.html',active="timeline")
+
 
 if __name__ == "__main__":
   app.run()
