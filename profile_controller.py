e5f6eb267c2046a774c5f798b6ecc8995476a2fb
Logan Cundiff <lcundiff@ufl.edu>
2019-07-24 15:23:58 -0400

    Iconic/resources page/bootstrap edits
    Implemented iconic into the app and took out bootstrap 3. Made a resources page template. connected pages together. made routing simpler.

--- profile_controller.py
+++ profile_controller.py
@@ -6,12 +6,15 @@ from flask import render_template
 
 
 @app.route('/')
+@app.route('/profile')
 def hello_name():    
-  return render_template('profile.html')
+  return render_template('profile.html') #this is the home page currently
 
-@app.route('/resources')
-def resources_page():
-  return render_template('resources.html')
+@app.route('/<page_to_load>')
+def resources_page(page_to_load):
+  #so href will add something to the url, and this will be saved to 'page_to_load' which we can then use to render the name of the html file
+  return render_template(page_to_load)
+
 
 if __name__ == "__main__":
   app.run()
