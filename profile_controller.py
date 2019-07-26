9f9c8515d0176064de8375600589a83bcba945ca
Logan Cundiff <lcundiff@ufl.edu>
2019-07-25 21:42:48 -0400

    Improvements to User class

--- profile_controller.py
+++ profile_controller.py
@@ -8,7 +8,7 @@ def load_users():
   users_messaging_you = ["lcundiff","acastro","acundiff"]
   for i in range(0,len(users_messaging_you)):
     testUser = User.getUser(users_messaging_you[i])
-    dict_of_users.update({testUser.name : testUser}) #key is username and the value is a User object
+    dict_of_users.update({testUser.username : testUser}) #key is username and the value is a User object
 
   return dict_of_users
 
@@ -24,7 +24,7 @@ def page_load(page_to_load):
   if(dict_of_users.get(page_to_load)): #if the url contains the user's name. (will change this to username or ID to not overlap)
     list_of_messages_in = dict_of_users.get(page_to_load).messages_in # get messages coming in
     list_of_messages_out = dict_of_users.get(page_to_load).messages_out # get messages going out 
-    return render_template('message.html', users = dict_of_users, user_messages_in = list_of_messages_in, user_messages_out = list_of_messages_out,)
+    return render_template('message.html', users = dict_of_users, user_messages_in = list_of_messages_in, user_messages_out = list_of_messages_out)
   
   return render_template(page_to_load, users = dict_of_users)
 
@@ -32,7 +32,7 @@ def page_load(page_to_load):
 @app.route('/')
 @app.route('/profile')
 def home():    
-  return render_template('profile.html') #this is the home page currently
+  return render_template('login.html') #this is the home page currently
 
 if __name__ == "__main__":
   app.run()
--- users.py
+++ users.py
@@ -6,17 +6,21 @@ from flask import render_template
 
 
 class User():
-  def __init__(user, name, messages_in, messages_out):
-    user.name = name
+  def __init__(user,fname, lname, username, messages_in, messages_out, role):
+    user.fname = fname
+    user.lname = lname
+    user.username = username 
     user.messages_in = messages_in
     user.messages_out = messages_out
+    user.role = role
+    
   def getUser(username):  
     testUser = test_user_data.get(username)
     return testUser
   
-test_user_data = {"lcundiff" : User("Logan",["how u doin", " wut?", "hahaha"],["what do prisoners use to talk to one another?","cell phones","lolol"]), 
-                  "acastro" : User("Ariel",["1st message would be here", " 2nd message would be here", "3rd message would be here"],["what do prisoners use to talk to one another?", "wut?", "cell phones"]),
-                  "acundiff" : User("Austin",["1st message would be here", " 2nd message would be here", "3rd message would be here"],["what do prisoners use to talk to one another?", "wut?", "cell phones"]),}
+test_user_data = {"lcundiff" : User("Logan", "Cundiff", "lcundiff", ["how u doin", " wut?", "hahaha"], ["what do prisoners use to talk to one another?","cell phones","lolol"], "admin"), 
+                  "acastro" : User("Ariel", "Castro", "acastro", ["1st message would be here", " 2nd message would be here", "3rd message would be here"], ["what do prisoners use to talk to one another?", "wut?", "cell phones"], "user"),
+                  "acundiff" : User("Austin", "Cundiff", "acundiff", ["1st message would be here", " 2nd message would be here", "3rd message would be here"], ["what do prisoners use to talk to one another?", "wut?", "cell phones"], "user")}
 
 
   
