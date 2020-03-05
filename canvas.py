5db4035398d63bc242e297ee3f9f0bd03c87298c
Logan Cundiff <lcundiff@ufl.edu>
2020-03-05 00:08:48 -0500

    update git ignore

--- canvas.py
+++ canvas.py
@@ -14,7 +14,7 @@ from requests import sessions
 global API_URL
 
 API_URL = "https://admin.p4hteach.org"
-ROCKET_URL = "https://p4h.rocket.chat/"
+ROCKET_URL = "https://open.rocket.chat/"
 
 # Canvas API key
 API_KEY = "e8MQdcca34Suq9XzwzYWTcec7oILoJ1558Y3b0fvVEgcOqx66DuwnCFvdo2CuK74"
