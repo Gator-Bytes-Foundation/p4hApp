cb5013696372996f96d828d5671f8672096152bf
MainShayne233 <shaynetremblay@gmail.com>
2020-04-27 22:44:56 -0400

    Add doc for static endpoint redirect

--- app/helpers/static_asset_resolve.py
+++ app/helpers/static_asset_resolve.py
@@ -2,6 +2,11 @@ from urllib.parse import urljoin
 from flask import redirect
 from app import app, environment
 
+##
+# In development, this will properly redirect any calls to `/static/*` (i.e. for a static asset) to the webpack dev server
+#
+# In production, the asset is served as it normally would.
+
 @app.endpoint("static")
 def static(filename):
     if environment == "development":
