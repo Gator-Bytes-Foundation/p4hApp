953d914c8c0c7b2eb86fac2e6fc67c5842bb9ef7
MainShayne233 <shaynetremblay@gmail.com>
2020-04-27 23:31:58 -0400

    Add comments

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,8 +1,5 @@
 /**
  * This is the main JS file that will get loaded on every page
- *
- * All page specific code should be explictly loaded in that template
- *
  */
 
 import "bootstrap/dist/css/bootstrap.min.css";
