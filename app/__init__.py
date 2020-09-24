41c0c0ed69d6762dbee25bfc8f6bf529533ac0ed
Logan Cundiff <lcundiff@ufl.edu>
2020-09-24 19:09:04 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-#file_upload = FileUpload(app, db)
+file_upload = FileUpload(app, db)
 
 
 environment = os.getenv('ENVIRONMENT', default='development')
