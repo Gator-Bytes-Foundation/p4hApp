0f73da524da25126e6cfa12d2c0bbd704bea92c3
Logan Cundiff <lcundiff@ufl.edu>
2020-11-01 17:00:15 -0500

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -17,7 +17,7 @@ const entries = glob.sync(path.resolve(".", "src/js/entry/*.js")).reduce(
   { main: path.resolve(".", "src/js/main.js") }
 );
 
-const outputPath = path.resolve(__dirname, "../static");
+const outputPath = path.resolve(__dirname, "../app/static");
 const assetPath = path.resolve(__dirname, "assets");
 
 module.exports = {
