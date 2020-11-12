74d6bcebd0dbd0dd86eed5ef51816a5ac573b213
Logan Cundiff <lcundiff@ufl.edu>
2020-11-12 13:10:47 -0500

    Update static_asset_resolve.py

--- app/helpers/static_asset_resolve.py
+++ app/helpers/static_asset_resolve.py
@@ -8,8 +8,8 @@ from app import app, environment
 # In production, the asset is served as it normally would.
 @app.endpoint("static")
 def static(filename):
-    if environment == "development":
-        assert_path = urljoin("http://localhost:9000", filename)
-        return redirect(assert_path)
-    else:
-        return app.send_static_file(filename)
+    #if environment == "development":
+    #   assert_path = urljoin("http://localhost:9000", filename)
+    #    return redirect(assert_path)
+    #else:
+    return app.send_static_file(filename)
