dbaec493fc2cccabad9f641338dd0c09f91d6086
Logan Cundiff <lcundiff@ufl.edu>
2020-12-13 14:45:23 -0500

    Revert "Revert "using flask default static file handling""
    This reverts commit bc6502ea9626c6737d639c1d64a13b8de4296ac3.

--- __main__.py
+++
@@ -1,5 +0,0 @@
-#from wsgi.py import application as app
-# This is to meet GLoud standard of having "main.py" file
-from app import app
-
-app.run(debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,11 @@ from app.canvas import Config
 
 import logging
 import sys
+<<<<<<< Updated upstream
 app = Flask(__name__,static_url_path="/app") #
+=======
+app = Flask(__name__,static_url_path="/app/static", static_folder="static")
+>>>>>>> Stashed changes
 
 app.config.from_object(Config)
 # Other FLASK config varaibles ...
