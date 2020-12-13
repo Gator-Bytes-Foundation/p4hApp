f6ee125cc264fa3d0edc650e1dbf7b0b2ffa65fd
Logan Cundiff <lcundiff@ufl.edu>
2020-12-13 14:46:14 -0500

    using flask default static file handling

--- app/helpers/static_asset_resolve.py
+++ app/helpers/static_asset_resolve.py
@@ -8,13 +8,6 @@ from app import app, environment
 # In production, the asset is served as it normally would.
 @app.endpoint("static")
 def static(filename):
-<<<<<<< Updated upstream
-    if environment == "development":
-        assert_path = urljoin("http://localhost:9000", filename)
-        return redirect(assert_path)
-    else:
-        return app.send_static_file(filename)
-=======
     #if(filename.contains('.css')) 
 
     print("STATIC " + filename)
@@ -23,4 +16,3 @@ def static(filename):
     #    return redirect(assert_path)
     #else:
     return app.send_static_file(filename)
->>>>>>> Stashed changes
