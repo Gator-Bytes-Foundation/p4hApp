ce744e68850465693b1ed72d301315dcf1de2ccf
Logan Cundiff <lcundiff@ufl.edu>
2022-11-21 21:45:19 -0500

    temp

---
+++ config/local.py
@@ -0,0 +1,6 @@
+from config.config import Default
+ENV="Development"
+class Config(Default):
+    DEBUG=True
+    CANVAS_URL="https://admin.p4hteach.org" #"http://192.168.1.50:3000"
+    ROCKET_URL="https://messaging.p4hteach.org"#"http://192.168.1.50:4000"
