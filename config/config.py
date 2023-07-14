24200abbb345a6c2516b5b15a2d85eaa5c62327c
Logan Cundiff <lcundiff@ufl.edu>
2023-07-13 21:32:32 -0400

    production gets db from env variable :ambulance:

--- config/config.py
+++ config/config.py
@@ -15,13 +15,13 @@ class Default(object):
 class ProductionConfig(Default):
     CANVAS_URL="https://admin.p4hteach.org"
     ROCKET_URL="https://messaging.p4hteach.org"
-    SQLALCHEMY_DATABASE_URI="postgres://pfcwbhgyuppdwi:65e697655f5decae0aa8bde291aa5d50446bda0c2a1e8e29fae341e739ee5bb3@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d99llu40rrukif"
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
 
 class StagingConfig(Default):
     DEBUG = True
     CANVAS_URL="https://admin.p4hteach.org"
     ROCKET_URL="https://messaging.p4hteach.org"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://ncyvtejtpjjajt:a8c930633cc4b7505c1f0dccb83c1f80e8cf1d54b8d72b54422aec7bf40ed57d@ec2-3-92-151-217.compute-1.amazonaws.com:5432/d6bf5h100s1387"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevelopmentConfig(Default):
     DEBUG = True
